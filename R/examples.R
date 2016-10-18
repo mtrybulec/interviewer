cb7afb84f7355959cc9cbb5ef70fde24a8aec022
mtrybulec <mtrybulec@yac.com.pl>
2016-10-18 14:21:50 +0100

    added runExample() documentation

--- R/examples.R
+++ R/examples.R
@@ -1,3 +1,11 @@
+#' Run one of the \code{interviewer} examples.
+#' 
+#' \code{runExample} runs one of the examples distributed wiht the \code{interviewer} package.
+#' 
+#' @param example (character) the directory name of one of the sub-directories in the pacakge's
+#'     \code{inst/examples/} directory. Call \code{runExample()} with no parameters to get a list
+#'     of valid examples.
+#'     
 #' @export
 runExample <- function(example) {
     examplesDir <- "examples"
