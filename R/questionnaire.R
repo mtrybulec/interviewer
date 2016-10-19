d0eab534ebaf313641b3a6473a413556d8f18c87
mtrybulec <mtrybulec@yac.com.pl>
2016-10-19 13:08:12 +0100

    domain reference removed

--- R/questionnaire.R
+++ R/questionnaire.R
@@ -3,9 +3,8 @@ questionnaire <- function(surveyId, userId, label, welcome, goodbye, onExit, ...
     domain <- shiny::getDefaultReactiveDomain()
     input <- domain$input
     output <- domain$output
-    session <- domain$session
 
-    pageContentId <- "pageContent"    
+    pageContentId <- "pageContent"
     initButtonId <- paste0(.buttonPrefix, "Init")
     backButtonId <- paste0(.buttonPrefix, "Back")
     nextButtonId <- paste0(.buttonPrefix, "Next")
@@ -25,11 +24,11 @@ questionnaire <- function(surveyId, userId, label, welcome, goodbye, onExit, ...
     shiny::isolate({
         context$page <- context$pages[[context$pageIndex]]
     })
-    
+
     shiny::observeEvent(input[[initButtonId]], {
         context$started <- TRUE
     })
-  
+
     shiny::observeEvent(input[[backButtonId]], {
         context$pageIndex <- context$pageIndex - 1
         context$page <- context$pages[[context$pageIndex]]
@@ -37,24 +36,24 @@ questionnaire <- function(surveyId, userId, label, welcome, goodbye, onExit, ...
 
     shiny::observeEvent(input[[nextButtonId]], {
         context$visitedPageIndexes <- unique(c(context$visitedPageIndexes, context$pageIndex))
-        
+
         validationFailed <- NULL
         validationResults <- context$validationResults
-        
+
         for (question in context$pages[[context$pageIndex]]$questions) {
             if (.isQuestion(question)) {
                 validationResult <- .validateResult(context, question)
-                
+
                 if ((validationResult != .validResult) && is.null(validationFailed)) {
                     validationFailed <- question$id
                 }
-                
+
                 validationResults[[question$id]] <- validationResult
             }
         }
-        
+
         context$validationResults <- validationResults
-        
+
         if (is.null(validationFailed)) {
             if (context$pageIndex >= length(context$pages)) {
                 context$data <- shiny::reactiveValuesToList(input)
@@ -74,38 +73,38 @@ questionnaire <- function(surveyId, userId, label, welcome, goodbye, onExit, ...
             data <- context$data
             data <- lapply(data, function(item) paste(item, collapse = ","))
             data <- as.data.frame(data)
-            
+
             names <- colnames(data)
             names <- sub(paste0("^", .questionPrefix), "", names)
             colnames(data) <- names
 
             data <- data[, intersect(context$questionOrder, names)]
-            
+
             onExit(data)
         }
     })
-    
+
     shiny::observeEvent(context$started, {
         shinyjs::toggle(initButtonId, condition = !context$started)
         shinyjs::toggle(backButtonId, condition = context$started)
         shinyjs::toggle(nextButtonId, condition = context$started)
     })
-    
+
     shiny::observe({
         shinyjs::toggleState(backButtonId, condition = context$started && (context$pageIndex > 1) && !context$done)
         shinyjs::toggleState(nextButtonId, condition = context$started && !context$done)
     })
-    
+
     output[[pageContentId]] <- shiny::renderUI({
         if (!context$started) {
             pageContent <- shiny::p(welcome)
         } else {
             pageContent <- context$page$ui(context)
         }
-        
+
         pageContent
     })
-    
+
     shiny::renderUI({
         if (context$done) {
             screenContent <- shiny::p(goodbye)
@@ -118,7 +117,7 @@ questionnaire <- function(surveyId, userId, label, welcome, goodbye, onExit, ...
                 shinyjs::hidden(shiny::actionButton(inputId = nextButtonId, label = "Next"))
             )
         }
-        
+
         list(
             shiny::h2(label),
             shiny::hr(),
