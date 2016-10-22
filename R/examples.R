f9d209f7b90f3144b7d04178e00737957bb52f5f
mtrybulec <mtrybulec@yac.com.pl>
2016-10-22 19:05:36 +0100

    added useInterviewer() documentation

--- R/examples.R
+++ R/examples.R
@@ -1,23 +1,23 @@
 #' Run one of the \code{interviewer} examples.
-#' 
+#'
 #' \code{runExample} runs one of the examples distributed wiht the \code{interviewer} package.
-#' 
+#'
 #' @param example (character) the directory name of one of the sub-directories in the pacakge's
 #'     \code{inst/examples/} directory. Call \code{runExample()} with no arguments to get a list
 #'     of valid examples.
-#'     
+#'
 #' @export
 runExample <- function(example) {
     examplesDir <- "examples"
     examples <- list.files(system.file(examplesDir, package = .packageName))
     exampleList <- sprintf("\n\nValid examples:\n  %s", paste(examples, collapse ="\n  "))
-    
+
     if (missing(example)) {
         stop(sprintf("Example name missing.%s", exampleList))
     }
-    
+
     appDir <- system.file(examplesDir, example, package = .packageName)
-  
+
     if (appDir == "") {
         stop(sprintf("Example '%s' not found.%s", example, exampleList))
     }
--- R/init.R
+++ R/init.R
@@ -1,7 +1,17 @@
+#' Set up \code{interviewer} in a Shiny app.
+#'
+#' \code{useInterviewer} should be called from a Shiny app's UI
+#'     in order for some of the other \code{interviewer} functions to work correctly.
+#'     It can be called from anywhere inside the UI.
+#'     It also calls \code{useShinyjs()} to set up \code{shinyjs}.
+#'
+#' @seealso
+#'     \code{\link{runExample}},
+#'     \code{\link[shinyjs]{useShinyjs}}.
 #' @export
 useInterviewer <- function() {
     shiny::addResourcePath(.packageName, system.file("www", package = .packageName))
-    
+
     jsFile <- file.path(.packageName, "main.js")
     cssFile <- file.path(.packageName, "main.css")
 
@@ -12,6 +22,6 @@ useInterviewer <- function() {
                 shiny::tags$script(src = jsFile),
                 shiny::tags$link(href = cssFile, rel = "stylesheet")
             )
-        )    
-    )    
+        )
+    )
 }
